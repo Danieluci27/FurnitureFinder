c20599d27e159149cb6e6ae1e2ebe0e2da68e522
Danieluci27 <yeonwos2@uci.edu>
2025-06-23 21:29:53 -0700

    Initial Commit

---
+++ FurnitureFinder/FurnitureFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureFinderApp.swift
+//  FurnitureFinder
+//
+//  Created by Daniel Shin on 6/23/25.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
